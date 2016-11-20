77e356e42de917a409edf0fdbe0133708766ded4
sureshreddy <sureshreddy228@gmail.com>
2016-11-20 08:46:40 +0530

    cosumer banking first commit

---
+++ src/main/java/banking/App.java
@@ -0,0 +1,14 @@
+package banking;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List<String> stringList = new ArrayList<String>();
+		System.out.println( "Hello World!" );
+    }
+}
